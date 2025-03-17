c7f694d6158f49cf19b434f67e4bce52de88bede
Komodo <144389222+Komodo-pty@users.noreply.github.com>
2025-03-17 13:52:40 -0700

    Add files via upload
    Added menu option for the Wordlist Mutator script

--- ares.sh
+++ ares.sh
@@ -3,7 +3,7 @@
 path=$(readlink $(which ares) | awk -F 'ares.sh' '{print $1}')
 while true;
 do
-	echo -e "\nSelect an operation:\n[0] Exit\n[1] Active Directory Roasting\n[2] XSS\n[3] MSSQL Brute Force\n"
+	echo -e "\nSelect an operation:\n[0] Exit\n[1] Active Directory Roasting\n[2] XSS\n[3] Wordlist Mutation\n[4] MSSQL Brute Force\n"
 	read mode
 
 	if [ $mode == 0 ]
@@ -19,6 +19,10 @@ do
 		python3 "$path"xss_cookie_thief.py
 
 	elif [ $mode == 3 ]
+	then
+		source "$path"password_generator.sh
+
+	elif [ $mode == 4 ]
 	then
 		source "$path"mssql_Brute.sh
 
